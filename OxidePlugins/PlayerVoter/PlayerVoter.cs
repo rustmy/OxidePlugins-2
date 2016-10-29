ce5e849456283e0cda11c0d0c270b2bdcdb1a4f6
exel80 <exel80p@gmail.com>
2016-10-29 17:20:50 +0300

    Update PlayerVoter.cs

--- OxidePlugins/PlayerVoter/PlayerVoter.cs
+++ OxidePlugins/PlayerVoter/PlayerVoter.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace OxidePlugins.PlayerVoter
+namespace Oxide.Plugins
 {
-    class PlayerVoter
+    class PlayerVoter : RustPlugin
     {
+
     }
 }
